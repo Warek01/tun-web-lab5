dfda1f323b1bbe241137da57fb2e15b1fc161a12
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2024-03-18 12:59:56 +0200

    Init lab 5

---
+++ Models/HttpCache.cs
@@ -0,0 +1,30 @@
+﻿namespace TumWebLab5.Models;
+
+public class HttpCache {
+  public class Entry {
+    public string   Url       { get; }
+    public string   Value     { get; }
+    
+    // TODO: timeout mechanism
+    // public DateTime CreatedAt { get; }
+    // public TimeSpan TTL   { get; }
+    // public bool     IsValid   { get; set; }
+
+    public Entry(string url, string value) {
+      Url   = url;
+      Value = value;
+    }
+  }
+
+  private readonly List<Entry> _entries = new();
+
+  public HttpCache() { }
+
+  public void Add(Entry entry) {
+    _entries.Add(entry);
+  }
+
+  public string? Get(string url) {
+    return _entries.Find(e => e.Url == url)?.Value;
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using TumWebLab5.Models;
+
+var url    = new CliParameter(args, "-u", true);
+var search = new CliParameter(args, "-s", true);
+var help   = new CliParameter(args, "-h", false);
+
+if (help.Found) {
+  Console.WriteLine(
+    """
+    go2web -u <URL>         # make an HTTP request to the specified URL and print the response
+    go2web -s <search-term> # make an HTTP request to search the term using your favorite search engine and print top 10 results
+    go2web -h               # show this help
+    """
+  );
+
+} else if (url is { Found: true, Value: not null }) {
+  var http = new HttpModule();
+  
+  Console.WriteLine(http.Get(url.Value));
+} else if (search is { Found: true, Value: not null }) {
+  throw new NotImplementedException();
+}
+
