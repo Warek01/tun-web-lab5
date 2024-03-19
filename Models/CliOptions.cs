b769e59d4cabbf8db2f9d1c0650cac844c14ad1b
Alexandru Dobrojan <alexandrudobrojan@gmail.com>
2024-03-19 16:07:53 +0200

    Add CLI options

---
+++ Models/CliOptions.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace TumWebLab5.Models;
+
+public class CliOptions {
+  [Option('u', "url", Required = false, HelpText = "Show page from URL")]
+  public string? Url { get; set; } = null;
+
+  [Option('s', "search", Required = false, HelpText = "Search with google")]
+  public string? Search { get; set; } = null;
+  
+  [Option('h', "help", Required = false, HelpText = "Get help")]
+  public bool Help { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,28 +1,33 @@
-﻿using System.Net;
+﻿using System.Globalization;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using CommandLine;
 using TumWebLab5.Models;
 
-var url    = new CliParameter(args, "-u", true);
-var search = new CliParameter(args, "-s", true);
-var help   = new CliParameter(args, "-h", false);
-var config = Config.Read("Config.json");
+using var cliParser = new Parser(s => {
+  s.AutoHelp               = false;
+  s.AutoVersion            = false;
+  s.CaseSensitive          = false;
+  s.IgnoreUnknownArguments = true;
+  s.ParsingCulture         = CultureInfo.InvariantCulture;
+});
+var config  = Config.Read("Config.json");
+var options = Parser.Default.ParseArguments<CliOptions>(args).Value;
 
-Utils.LogError("Test", new Exception());
-
-if (help.Found) {
+if (options.Help) {
   Console.WriteLine(
     """
-    go2web -u <URL>         # make an HTTP request to the specified URL and print the response
-    go2web -s <search-term> # make an HTTP request to search the term using your favorite search engine and print top 10 results
-    go2web -h               # show this help
+    go2web -u --url <URL>            Make an HTTP request to the specified URL and print the response
+    go2web -s --search <search-term> Make an HTTP request to search the term using your favorite search engine and print top 10 results
+    go2web -h --help                 Show this help
     """
   );
-} else if (url is { Found: true, Value: not null }) {
+} else if (options.Url != null) {
   var http = new HttpModule();
 
-  var message = http.Get(url.Value);
+  var message = http.Get(options.Url);
   Console.WriteLine(message);
-} else if (search is { Found: true, Value: not null }) {
+} else if (options.Search != null) {
   throw new NotImplementedException();
 }
